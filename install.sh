cea785bd59eb9f895d0f79f862f14f1ec5862471
Adly Abdullah <adly_abdullah@cention.asia>
2014-04-29 10:57:38 +0800

    Allow external scripts to modify MODULE_PATH in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-MODULE_PATH=`ferite --help | grep 'Base Cross Platform Library' | cut -d: -f2`
+if [ "$MODULE_PATH" == "" ];then
+	MODULE_PATH=`ferite --help | grep 'Base Cross Platform Library' | cut -d: -f2`
+fi
 MODULES=`ls *.fe`
 
 cp $MODULES $MODULE_PATH
