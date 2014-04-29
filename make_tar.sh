c1529c5cc81bbb6476b8472f51e628a892d7f484
Adly Abdullah <adly_abdullah@cention.asia>
2014-04-29 12:04:28 +0800

    Created farm.yard file

--- make_tar.sh
+++ make_tar.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 mkdir ferite-module-webserver
-cp *.fe *.sh ferite-module-webserver
+cp *.fe *.sh *.yard ferite-module-webserver
 tar cvzf ferite-module-webserver.tar.gz ferite-module-webserver
 rm -r ferite-module-webserver
