b14ca12ed26df3672125e532526c25265fa79a47
Adly Abdullah <adly_abdullah@cention.asia>
2014-04-29 10:58:25 +0800

    Include shell scripts in tar.gz

--- make_tar.sh
+++ make_tar.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 mkdir ferite-module-webserver
-cp *.fe ferite-module-webserver
+cp *.fe *.sh ferite-module-webserver
 tar cvzf ferite-module-webserver.tar.gz ferite-module-webserver
 rm -r ferite-module-webserver
